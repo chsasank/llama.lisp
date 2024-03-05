e79297e3a10c265856298fc0223e4fcbee09ea25
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2024-03-05 22:26:47 +0530

    changed the optimization scheme; lot more flexible now

--- src/rewrite.lisp
+++ src/rewrite.lisp
@@ -6,38 +6,68 @@
 (load "pat-match.lisp")
 (load "macros.lisp")
 
-;; Helper functions for FL rules
-(defparameter *fl-equations* nil
+;;; Let's create a different structure
+;;; No RHS and stuff
+(defparameter *fl-rewrites* nil 
   "A list of all rules available for rewrite")
 
-(defun reverse-ref (ref)
-  (concatenate 'string ref "-rev"))
-
-(defun add-fl-equation (&key lhs rhs ref)
-  "Add reference and its reverse to the rulebase"
-  (pushnew (cons (intern ref) (list lhs rhs)) *fl-equations*)
-  (pushnew (cons (intern (reverse-ref ref)) (list rhs lhs))
-            *fl-equations*))
-
-(defun fl-equation (ref)
-  (assoc (intern ref) *fl-equations*))
-
 (defun fl-rewrite (ref)
-  (cdr (fl-equation ref)))
+  "Get rewrite for given reference"
+  (second (cdr (assoc (intern ref) *fl-rewrites*))))
 
-;; Add rules
-(add-fl-equation
-  :ref "2-49"
-  :lhs '(comp trans
-          (for-loop ?j ?r ?s
-            (for-loop ?i ?f ?g ?E)))
-  :rhs '(for-loop ?i ?f ?g
-          (for-loop ?j ?r ?s ?E)))
+(defun fl-pattern (ref)
+  "Get pattern for given reference"
+  (first (cdr (assoc (intern ref) *fl-rewrites*))))
 
-(add-fl-equation
-  :ref "2-53"
-  :lhs '(comp (alpha ?f) (for-loop ?i ?g ?h ?E))
-  :rhs '(for-loop ?i ?g ?h (comp ?f ?E)))
+(defun add-fl-rewrite (&key pattern rewrite ref)
+  "Add reference and its reverse to the rulebase"
+  (pushnew (cons (intern ref) (list pattern rewrite)) *fl-rewrites*))
+
+(add-fl-rewrite
+  :pattern '(comp (const (?is ?i integerp))
+                  (cat (?* ?fn-list)))
+  :rewrite #'(lambda (binding)
+    (nth (lookup '?i binding) (lookup '?fn-list binding)))
+  :ref "2:3")
+
+(add-fl-rewrite
+  :pattern '(comp ?inside-comp)
+  :rewrite #'(lambda (binding)
+    (lookup '?inside-comp binding))
+  :ref "2-21-0")
+
+(add-fl-rewrite
+  :pattern '(comp (?* ?start-comp)
+              (comp (?* ?inside-comp))
+              (?* ?end-comp))
+  :rewrite #'(lambda (binding)
+    `(comp ,@(lookup '?start-comp binding)
+           ,@(lookup '?inside-comp binding)
+           ,@(lookup '?end-comp binding)))
+  :ref "2-21")
+
+(add-fl-rewrite
+  :pattern '(comp (cat (?* ?fn-list) ?g))
+  :rewrite #'(lambda (binding)
+    `(cat ,@(mapcar
+              #'(lambda (f) (comp f (lookup '?g binding)))
+              (lookup '?fn-list binding))))
+  :ref "2:25")
+
+(add-fl-rewrite
+  :pattern '(comp trans
+              (for-loop ?j ?r ?s
+                (for-loop ?i ?f ?g ?E)))
+  :rewrite #'(lambda (binding)
+    (sublis binding '(for-loop ?i ?f ?g
+                        (for-loop ?j ?r ?s ?E))))
+  :ref "2-49")
+
+(add-fl-rewrite
+  :pattern '(comp (alpha ?f) (for-loop ?i ?g ?h ?E))
+  :rewrite #'(lambda (binding)
+    (sublis binding '(for-loop ?i ?g ?h (comp ?f ?E))))
+  :ref "2-53")
 
 ;; Normalize comps for faster converge`nce
 (defun normalize-comp-step (prog)
@@ -45,19 +75,13 @@
   Ideally should be derivable from above."
   (if (not (listp prog))
     prog
-    (let ((binding-simple (pat-match '(comp ?inside-comp) prog)))
+    (let ((binding-simple (pat-match (fl-pattern "2-21-0") prog)))
       (if binding-simple
-        (cdr (assoc 'inside-comp binding-simple))
+        (funcall (fl-rewrite "2-21-0") binding-simple)
         ; try yet another pattern
-        (let* ((comp-pat '(comp (?* ?start-comp)
-                              (comp (?* ?inside-comp))
-                              (?* ?end-comp)))
-               (binding (pat-match comp-pat prog)))
+        (let ((binding (pat-match (fl-pattern "2-21") prog)))
           (if binding
-            `(comp ,@(cdr (assoc '?start-comp binding))
-                  ,@(cdr (assoc '?inside-comp binding))
-                  ,@(cdr (assoc '?end-comp binding)))
-            
+            (funcall (fl-rewrite "2-21") binding)
             ; try recursively if this too failed
             (mapcar #'normalize-comp-step prog)))))))
 
@@ -72,7 +96,7 @@
 
 (defun check-if-comp-based-rule (rule)
   "Check if rule is comp based"
-  (eq (first (first rule)) 'comp))
+  (eq (first (fl-pattern rule)) 'comp))
 
 (defun apply-comp-based-rule (rule prog)
   "Apply composition based rules.
@@ -82,30 +106,26 @@
     (error "rule ~a doesn't start with comp" rule))
 
   (let* ((comp-pat `(comp (?* ?start-comp)
-                          ,@(rest (first rule))
+                          ,@(rest (fl-pattern rule))
                           (?* ?end-comp)))
          (binding (pat-match comp-pat prog)))
     (if binding
       `(comp ,@(cdr (assoc '?start-comp binding))
-             ,(sublis binding (second rule))
+             ,(funcall (fl-rewrite rule) binding)
              ,@(cdr (assoc '?end-comp binding)))
         prog)))
 
 (defun apply-rule (rule prog)
   (if (check-if-comp-based-rule rule)
     (apply-comp-based-rule rule prog)
-    (let ((bindings (pat-match (first rewrite-rule) prog)))
-      (if bindings
-        (sublis bindings (second rewrite-rule)))
-        prog)))
+    (let ((bindings (pat-match (fl-pattern rule) prog)))
+      (if bindings (funcall (fl-rewrite rule) bindings) prog))))
 
 (defun apply-rule-recursively (rule prog)
   (if (not (listp prog))
     prog
     (let ((new-prog (apply-rule rule prog)))
       (if (equal new-prog prog)
-        ; don't give up yet
         (mapcar #'(lambda (subprog)
-                    (apply-rule-recursively rule subprog))
-          prog)
+                    (apply-rule-recursively rule subprog)) prog)
         new-prog))))
--- src/rewrite.test.lisp
+++ src/rewrite.test.lisp
@@ -1,14 +1,12 @@
 (load "rewrite.lisp")
 
 (fl-let 'IP '(comp (insert add) (alpha mul) trans))
-(fl-let 'MM '(comp (alpha (alpha IP) (alpha distl) distr 
+(fl-let 'MM '(comp (alpha (alpha IP) (alpha distl) distr
                  (cat (idx 0) (comp trans (idx 1))))))
 (fl-let 'C-IP
     '(for-loop i1 (const 0) (const 2)
         (for-loop i2 (const 0) (comp len (idx 0))
                     (comp (idx i2) (idx i1)))))
-
-(trace normalize-comp)
 
 (defvar *test-cases* `(
   ; function, expected value
@@ -27,7 +25,7 @@
   ((normalize-comp '(comp f1 (comp f2 (comp f3 f4) f5)))
    (comp f1 f2 f3 f4 f5))
 
-  ((normalize-comp '(comp g (alpha 
+  ((normalize-comp '(comp g (alpha
                       (comp f1 (comp f2 (comp f3 f4) f5)))))
    (comp g (alpha (comp f1 f2 f3 f4 f5))))
 
@@ -37,32 +35,41 @@
       (for-loop i2 (const 0) (comp len (idx 0))
         (comp (idx i2) (idx i1))))))
 
-  ((apply-comp-based-rule (fl-rewrite "2-53") 
+  ((apply-comp-based-rule "2-53"
     '(comp (insert add) (alpha mul)
         (for-loop i1 (const 0)
           len (idx i1))))
    (comp (insert add)
      (for-loop i1 (const 0) len
       (comp mul (idx i1)))))
-  
+
   ; without recursion, this rule should fail
-  ((apply-rule (fl-rewrite "2-53")
+  ((apply-rule "2-53"
     '(comp (insert add) (comp (alpha f) (for-loop i g h E))))
    (comp (insert add) (comp (alpha f) (for-loop i g h E))))
 
-  ((normalize-comp '(comp (insert add) (comp 
+  ; rule passes with recursion
+  ((apply-rule-recursively "2-53"
+    '(comp (insert add) (comp (alpha f) (for-loop i g h E))))
+   (comp (insert add) (comp (for-loop i g h (comp f E)))))
+
+  ; new rule for normalization
+  ((normalize-comp '(comp (insert add) (comp
                      (for-loop i g h (comp f E)))))
-    (comp (insert add) (for-loop i g h (comp f E))))
-  
-  ((normalize-comp (apply-rule-recursively (fl-rewrite "2-53")
-    '(comp (insert add) (comp (alpha f) (for-loop i g h E)))))
    (comp (insert add) (for-loop i g h (comp f E))))
 
-  ; ((apply-rule-recursively (fl-rewrite "2-53")
-  ;   (normalize-comp (fl-expand '(comp IP C-IP))))
-  ;  ,(normalize-comp (fl-expand '(comp IP C-IP))))
+  ; this rule doesn't work without 2-49
+  ((apply-rule-recursively "2-53"
+    (normalize-comp (fl-expand '(comp IP C-IP))))
+   ,(normalize-comp (fl-expand '(comp IP C-IP))))
 
-
+  ; see [2], eq 49
+  ((apply-rule-recursively "2-49"
+    (normalize-comp (fl-expand '(comp IP C-IP))))
+   (comp (insert add) (alpha mul)
+    (for-loop i2 (const 0) (comp len (idx 0))
+      (for-loop i1 (const 0) (const 2)
+        (comp (idx i2) (idx i1))))))
 ))
 
 (defun test-driver ()
@@ -78,15 +85,4 @@
             expression expected actual))))
     *test-cases*))
 
-
-; (let ((prog (fl-expand '(comp IP HY))))
-;   (print prog)
-;   (print "Original program")
-;   (print (normalize-comp prog))
-;   ; (print 
-;   ;   (mapcar
-;   ;     #'(lambda (x) (rewrite-program prog x))
-;   ;     (generate-rewrite-rules '1-def-comp)))
-      
-; )
 (test-driver)
