78cec1a83e781f48f4f6f6e45ab83603d28cfce0
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2024-06-10 12:46:22 +0530

    Use random names for temporary variables

--- src/backend/c-lisp.scm
+++ src/backend/c-lisp.scm
@@ -1,8 +1,10 @@
 ;; Result variable numbering
-(define tmp-num 0)
 (define (tmp-sym)
-    (set! tmp-num (+ tmp-num 1))
-    (string->symbol (string-append "tmp_" (number->string tmp-num))))
+    (string->symbol (string-append
+        "tmp_c-lisp_"
+        (number->string (+ (random 899999) 100000))))) ; a 5-digit number
+(define res-sym (make-object-property)) ; Holds the result of a block of instructions
+
 
 ;; Utility accessors
 (define (first expr) (list-ref expr 0))
@@ -18,14 +20,14 @@
     (assq-ref arith-ops expr))
 
 (define (emit-set name type op args)
-    `((set (,name ,type) ,(append `(,op) args))))
+    (define ins `((set (,name ,type) ,(append `(,op) args))))
+    (set! (res-sym ins) name)
+    ins)
 
 
 ;; Procedures to emit code
 ;; Each procedure here returns a list containing complete Brilisp
 ;; instructions (even if it's just one instruction).
-;; Each emitted list takes inputs from `tmp_*` variables and puts
-;; its result in the variable `res`
 (define (emit-expr expr)
     (cond
         ((symbol? expr) (emit-id-expr expr))
@@ -38,37 +40,40 @@
           (arg1 (emit-expr (list-ref expr 1)))
           (arg2 (emit-expr (list-ref expr 2)))
           (err (if (eq? (length expr) 3) 0 (error "Arithmetic operations take exactly 2 arguments:" expr)))
-          (in1 (tmp-sym))
-          (in2 (tmp-sym)))
+          (res (tmp-sym)))
 
-        (append!
+        (define ins (append
             arg1
-            (emit-set in1 'int 'id `(res))
             arg2
-            (emit-set in2 'int 'id `(res))
-            (emit-set 'res 'int op `(,in1 ,in2)))))
+            (emit-set res 'int op `(,(res-sym arg1) ,(res-sym arg2)))))
+        (set! (res-sym ins) res)
+        ins))
 
 (define (emit-id-expr expr)
-    (emit-set 'res 'int 'id `(,expr)))
+    (emit-set (tmp-sym) 'int 'id `(,expr)))
 
 (define (emit-const-int-expr expr)
-    (emit-set 'res 'int 'const `(,expr)))
+    (emit-set (tmp-sym) 'int 'const `(,expr)))
 
 (define (emit-call-expr expr)
     (define arg-syms '())
-    (define arg-ins '())
+    (define arg-ins-list '())
+    (define res (tmp-sym))
     (for-each
         (lambda (a)
             (define arg-sym (tmp-sym))
+            (define arg-ins (emit-expr a))
             (set! arg-syms (append arg-syms `(,arg-sym)))
-            (set! arg-ins (append
+            (set! arg-ins-list (append
+                arg-ins-list
                 arg-ins
-                (emit-expr a)
-                (emit-set arg-sym 'int 'id '(res)))))
+                (emit-set arg-sym 'int 'id `(,(res-sym arg-ins))))))
         (list-tail expr 1))
-    (append
-        arg-ins
-        (emit-set 'res 'int 'call (append `(,(first expr)) arg-syms))))
+    (define ins (append
+        arg-ins-list
+        (emit-set res 'int 'call (append `(,(first expr)) arg-syms))))
+    (set! (res-sym ins) res)
+    ins)
 
 (display
     (emit-expr
