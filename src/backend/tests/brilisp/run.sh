8577672686da44cb2802d69fa2698f5039c2fc11
Vedanth Padmaraman <vedanthpadmaraman0566@gmail.com>
2024-07-03 15:43:39 +0530

    Use command `cat` instead of file `/dev/stdin` for stdin access

--- src/backend/tests/brilisp/run.sh
+++ src/backend/tests/brilisp/run.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 tmp_in=$(mktemp --suffix '.ll')
 tmp_out=$(mktemp --suffix '.out')
 
-cp /dev/stdin $tmp_in
+cat > $tmp_in
 clang $tmp_in runtime.c -o $tmp_out -Wno-override-module -O2
 $tmp_out $@
 
