48d44268b7591c329123576715eb6bb2efcffcab
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-07-12 14:59:21 +0530

    returns 0 is NaN

--- src/backend/tests/c-lisp/kernel/runtime/bench.c
+++ src/backend/tests/c-lisp/kernel/runtime/bench.c
@@ -6,9 +6,9 @@
 double flops(int m, int n, int k, double time) {
     // printf("%.3f\n", time);      /** FOR DEBUGGING */
     double num_ops = 2.0 * (double)m * (double)n * (double)k;
-    return num_ops / (time * 1e9);
+    double result = num_ops / (time * 1e9);
+    return isnan(result) ? 0 : result;
 }
-
 
 clock_t timer() {
     return clock(); 
