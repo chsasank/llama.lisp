09df7597adb12f607815b81032ae5a90b34da482
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-07-16 12:05:19 +0530

    change compiling and linking routine

--- src/backend/tests/c-lisp/kernel/compile.sh
+++ src/backend/tests/c-lisp/kernel/compile.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "No input argument provided."
+if [ $# -eq 0 ]; then
+    echo "No input arguments provided."
     exit 1
 fi
 
 build_dir=build
-input_file="$1.sexp"
-kernel_object="$1.o"
-ir_file="$1.ll"
-executable_file="$1"
-
-if [ ! -e $input_file ]; then
-    echo "File '$input_file' not found."
-    exit 1
-fi
 
 if [ ! -d "$build_dir" ]; then
     echo "Creating build directory..."
     mkdir "$build_dir"
 fi
 
-guile ../../../utils/sexp-json.scm < $input_file \
-  | python ../../../c-lisp.py \
-  | python ../../../brilisp.py \
-  | python ../../../llvm.py > build/$ir_file 
+for input in "$@"; do
+    input_file="$input.sexp"
+    kernel_object="$input.o"
+    ir_file="$input.ll"
+    executable_file="$input"
 
-clang -O1 -Wno-implicit-function-declaration -c -o build/$kernel_object build/$ir_file
+    if [ ! -e $input_file ]; then
+        echo "File '$input_file' not found."
+        exit 1
+    fi
+
+    guile ../../../utils/sexp-json.scm < $input_file \
+      | python ../../../c-lisp.py \
+      | python ../../../brilisp.py \
+      | python ../../../llvm.py > build/$ir_file 
+
+    clang -O1 -Wno-implicit-function-declaration -c -o build/$kernel_object build/$ir_file
+done
+
 clang -O1 -Wno-implicit-function-declaration -c -o build/main.o runtime/main.c
 clang -O1 -Wno-implicit-function-declaration -c -o build/matrix.o runtime/matrix.c
-clang -O1 -Wno-implicit-function-declaration -o build/$executable_file build/main.o build/matrix.o build/$kernel_object
 
+objects="build/main.o build/matrix.o"
+for input in "$@"; do
+    kernel_object="$input.o"
+    objects="$objects build/$kernel_object"
+done
+
+clang -O1 -Wno-implicit-function-declaration -o build/kernel_bench $objects
+
+if [ $? -eq 0 ]; then
+    echo "Build successful. Executable created: build/output_executable"
+else
+    echo "Build failed."
+    exit 1
+fi
