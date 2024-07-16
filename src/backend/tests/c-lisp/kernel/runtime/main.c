8926ddc94e72bb3e356e8fdd1bc378a51d900c38
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-07-16 20:34:58 +0530

    change how then benchmark routine is run

--- src/backend/tests/c-lisp/kernel/runtime/main.c
+++ src/backend/tests/c-lisp/kernel/runtime/main.c
@@ -1,100 +1,102 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/time.h>
 
+#include "parameters.h"
 #include "print.c"
-// #include "matrix.c"
 #include "bench.c"
 
-int main(){
+int main(int argc, char* argv[]){
     int m, n, k;
 
-    // m = 2000;
-    // n = 2000;
-    // k = 2000;
-    
-    printf("size, is_equal,kernel_time,kernel_gflops,ref_time,ref_gflops\n");
-    int size = 0;
-    for (size = 0; size <= 2600; size += 200){
-        m = size;
-        n = size;
-        k = size;
+    char* routine = argv[1];
 
-        float* A = (float*)malloc(m * k * sizeof(float));
-        float* B = (float*)malloc(k * n * sizeof(float));
-        float* C_kernel = (float*)malloc(m * n * sizeof(float));
-        float* C_ref = (float*)malloc(m * n * sizeof(float));
+    if(!strcmp(routine, "once")) {
+        m = M;
+        n = N;
+        k = K;  
+    
+        float* A          =    (float*)malloc(m * k * sizeof(float));
+        float* B          =    (float*)malloc(k * n * sizeof(float));
+        float* C_kernel   =    (float*)malloc(m * n * sizeof(float));
+        float* C_ref      =    (float*)malloc(m * n * sizeof(float));
 
         random_matrix(A, m, k);
         random_matrix(B, k, n);
 
-
         clock_t ref_start = clock();
-        ref_mult(A, B, C_ref, m, n, k);
-        // ref_mult(A, B, C_ref, m, n, k);
-        // ref_mult(A, B, C_ref, m, n, k);
+        kernel(A, B, C_ref, m, n, k);
         clock_t ref_end = clock();
 
         clock_t kernel_start = clock();
-        // __MMult1(A, B, C_kernel, m, n, k);
-        // __MMult1(A, B, C_kernel, m, n, k);
-        __MMult1(A, B, C_kernel, m, n, k);
+        __kernel(A, B, C_kernel, m, n, k);
         clock_t kernel_end = clock();
 
+        double elapsed_kernel = (double)(kernel_end - kernel_start) / CLOCKS_PER_SEC;
+        double elapsed_ref = (double)(ref_end - ref_start) / CLOCKS_PER_SEC;
 
-        bool isequal = compare_matrix(C_kernel, C_ref, m, n);
+        float kernel_flops = flops(m, n, k, elapsed_kernel);
+        float ref_flops = flops(m, n, k, elapsed_ref);
 
-        double elapsed_kernel = (double) (kernel_end - kernel_start) / CLOCKS_PER_SEC;
-        double elapsed_ref = (double) (ref_end - ref_start) / CLOCKS_PER_SEC;
+        bool isequal = compare_matrix((float*)C_kernel, (float*)C_ref, m, n);
+        bool allclose = all_close((float*)C_kernel, (float*)C_ref, m, n, 1e3, 1e5, true);
 
-        float kernel_flops = flops(size, size, size, elapsed_kernel);
-        float ref_flops = flops(size, size, size, elapsed_ref);
-
-        printf("%d, %d, %.3f, %.3f, %.3f, %.3f\n",
-                size, isequal,
-                elapsed_kernel,
-                kernel_flops,
-                elapsed_ref,
-                ref_flops
-              );
+        printf("%d, %.3f, %.3f\n", allclose, ref_flops, kernel_flops);
 
         fflush(stdout);
-        free(A);
-        free(B);
-        free(C_ref);
-        free(C_kernel);
-    }
 
-    // volatile float* A          =    (float*)malloc(m * k * sizeof(float));
-    // volatile float* B          =    (float*)malloc(k * n * sizeof(float));
-    // volatile float* C_kernel   =    (float*)malloc(m * n * sizeof(float));
-    // volatile float* C_ref      =    (float*)malloc(m * n * sizeof(float));
+        free((float*)A);
+        free((float*)B);
+        free((float*)C_kernel);
+        free((float*)C_ref);
+    
+    } else if (!strcmp(argv[1], "many")) {
+        for(int size = START; size <= END; size += STEP) {
+           
+            m = n = k = size;
 
-    // clock_t ref_start = clock();
-    // ref_mult((float*)A, (float*)B, (float*)C_ref, m, n, k);
-    // clock_t ref_end = clock();
+            for(int j = 0; j < SAMPLES; j++){
+                float* A          =    (float*)malloc(m * k * sizeof(float));
+                float* B          =    (float*)malloc(k * n * sizeof(float));
+                float* C_kernel   =    (float*)malloc(m * n * sizeof(float));
+                float* C_ref      =    (float*)malloc(m * n * sizeof(float));
 
-    // clock_t kernel_start = clock();
-    // __MMult1((float*)A, (float*)B, (float*)C_kernel, m, n, k);
-    // clock_t kernel_end = clock();
+                random_matrix(A, m, k);
+                random_matrix(B, k, n);
 
-    // double elapsed_kernel = (double)(kernel_end - kernel_start) / CLOCKS_PER_SEC;
-    // double elapsed_ref = (double)(ref_end - ref_start) / CLOCKS_PER_SEC;
 
-    // float kernel_flops = flops(m, n, k, elapsed_kernel);
-    // float ref_flops = flops(m, n, k, elapsed_ref);
+                clock_t ref_start = clock();
+                kernel(A, B, C_ref, m, n, k);
+                clock_t ref_end = clock();
 
-    // bool isequal = compare_matrix((float*)C_kernel, (float*)C_ref, m, n);
+                clock_t kernel_start = clock();
+                __kernel(A, B, C_kernel, m, n, k);
+                clock_t kernel_end = clock();
 
-    // printf("kernel flops : %.3f\n", kernel_flops);
-    // printf("ref flops    : %.3f\n", ref_flops);
+                double elapsed_kernel = (double)(kernel_end - kernel_start) / CLOCKS_PER_SEC;
+                double elapsed_ref = (double)(ref_end - ref_start) / CLOCKS_PER_SEC;
 
-    // free((float*)A);
-    // free((float*)B);
-    // free((float*)C_kernel);
-    // free((float*)C_ref);
+                float kernel_flops = flops(m, n, k, elapsed_kernel);
+                float ref_flops = flops(m, n, k, elapsed_ref);
+
+                bool isequal = compare_matrix((float*)C_kernel, (float*)C_ref, m, n);
+                bool allclose = all_close((float*)C_kernel, (float*)C_ref, m, n, 1e3, 1e5, true);
+
+                printf("%d, %d, %.3f, %.3f\n", size,  allclose, ref_flops, kernel_flops);
+                
+                fflush(stdout);
+
+                free((float*)A);
+                free((float*)B);
+                free((float*)C_kernel);
+                free((float*)C_ref);
+            }
+        }
+
+    } else { printf("Invalid Input Argument\n"); }
 
     return 0;
 }
