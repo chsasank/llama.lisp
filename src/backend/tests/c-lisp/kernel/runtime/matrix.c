5b93eb796d130cdd2b7fb5db8b57fb80f4729f86
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-07-19 02:26:05 +0530

    clean up

--- src/backend/tests/c-lisp/kernel/runtime/matrix.c
+++ src/backend/tests/c-lisp/kernel/runtime/matrix.c
@@ -14,59 +14,6 @@ int random_matrix(float* matrix, int rows, int cols) {
     return 1;
 }
 
-void add_dot(int k, float* a, int incx ,float* b, float* c) {
-    float a_value, b_value;
-    float acc = 0.;
-
-    for(int p = 0; p < k; p++) {
-        a_value = a[p * incx];
-        b_value = b[p];
-        acc += a_value * b_value;
-    }
-    c[0] = acc;
-}
-
-// int ref_MMult1(float* A, float* B, float* C, int m, int n, int k) {
-//     for (int i = 0; i < m; i++) {
-//         for (int j = 0; j < n; j++) {
-//             float sum = 0.0;
-//             for (int p = 0; p < k; p++) {
-//                 sum += A[p * m + i] * B[j * k + p];
-//             }
-//             C[j * m + i] = sum;
-//         }
-//     }
-//     return 1;
-// }
-
-// void add_dot1x4(int k, float* a, int lda, float* b, int ldb, float* c, int ldc) {
-//     c[0 * ldc] = 0.;
-//     c[1 * ldc] = 0.;
-//     c[2 * ldc] = 0.;
-//     c[3 * ldc] = 0.;
-
-//     int p;
-
-//     for(p = 0; p < k; p++)
-//         c[0 * ldc] += a[p * lda] * b[p + ldb * 0];
-
-//     for(p = 0; p < k; p++)
-//         c[1 * ldc] += a[p * lda] * b[p + ldb * 1];
-
-//     for(p = 0; p < k; p++)
-//         c[2 * ldc] += a[p * lda] * b[p + ldb * 2];
-
-//     for(p = 0; p < k; p++)
-//         c[3 * ldc] += a[p * lda] * b[p + ldb * 3];
-
-// }       
-
-// void ref_MMult_1x4(int m, int n, int k, int lda, int ldb, int ldc, float* a, float* b, float* c) {
-//     for(int j = 0; j < n; j += 4) 
-//         for(int i = 0; i < m; i++) 
-//             add_dot1x4(k, &a[i], lda, &b[j * ldb], ldb, &c[j * ldc + i], ldc);
-// }
-
 bool compare_matrix(float* res, float* ref, int rows, int cols) {
     for (int j = 0; j < cols; j++) {
         for (int i = 0; i < rows; i++) {
