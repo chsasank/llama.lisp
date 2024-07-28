e83f38444b3e8c6dc9742b49e7d516fbdcb0f197
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-07-28 20:38:40 +0530

    MMult_4x4_5 : add macros

---
+++ src/backend/tests/c-lisp/kernel/kernels_c-lisp/MMult_4x4_5.py
@@ -0,0 +1,15 @@
+def init_c(c, ldc, row):
+    return [
+        ("store", ("ptradd", c, ("add", row, ("mul", ele, ldc))), 0.0)
+        for ele in range(3)
+    ]
+
+def elemental_muladd_4x4(a, b, c, lda, ldb, ldc, loop_var, row):
+    elemental_muladd = [
+            ('store', ('ptradd', c, ('add', row, ('mul', ele, ldc))),
+                   ('fadd', ('load', ('ptradd', c, ('add', row, ('mul', ele, ldc)))),
+                         ('fmul', ('load', ('ptradd', a, ('add', 0, ('mul', loop_var, lda)))),
+                               ('load', ('ptradd', b, ('add', loop_var, ('mul', ele, ldb)))))))
+            for ele in range(3)
+    ]
+    return elemental_muladd
