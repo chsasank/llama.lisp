51b0d247886d0b195dc617bbf64682cddf1895e1
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-07-28 20:40:09 +0530

    change benchmarking parameters

--- src/backend/tests/c-lisp/kernel/runtime/parameters.h
+++ src/backend/tests/c-lisp/kernel/runtime/parameters.h
@@ -1,8 +1,8 @@
-#define START 200
+#define START 400
 #define END 2000
-#define STEP 200
+#define STEP 400
 #define SAMPLES 10
 
-#define M 1000
-#define N 1000
-#define K 1000
+#define M 8
+#define N 8
+#define K 8
