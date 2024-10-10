b0d21809ad1e83360e744041a3fb20f50dd97777
adi-lb-phoenix <adithyabhatl1998@gmail.com>
2024-10-10 18:58:38 +0530

    lisp installation script for docuseal application

---
+++ src/app-store/definitions/docuseal/docuseal.lisp
@@ -0,0 +1,26 @@
+(define-app
+    (version "1.7.5")
+    (ports 3000)
+    (let ((db_password ,(gen-password)))
+        (containers
+            (container 
+                (name "docuseal")
+                (image "docker.io/docuseal/docuseal:latest")
+                (volumes 
+                    ("docuseal" "/data/docuseal"))
+                (environment
+                    ("FORCE_SSL" "false")
+                    ("DATABASE_HOST" "localhost")
+                    ("DATABASE_PORT" "5432")
+                    ("DATABASE_USER" "postgres")
+                    ("DATABASE_PASSWORD" ,db_password)
+                    ("DATABASE_NAME" "docuseal")))
+            (container 
+                (name "postgres")
+                (image "docker.io/postgres:15")
+                (volumes 
+                    ("pg_data" "/var/lib/postgresql/data"))
+                (environment 
+                    ("POSTGRES_USER" "postgres")
+                    ("POSTGRES_PASSWORD" ,db_password)
+                    ("POSTGRES_DB" "docuseal"))))))
