c36e4e375633363f931bcfc3a8c17cc8e4c6ab5c
adi-lb-phoenix <adithyabhatl1998@gmail.com>
2024-10-11 16:29:35 +0530

    mades some changes to refelect granularity

--- src/app-store/definitions/uptime-kuma/uptime-kuma.lisp
+++ src/app-store/definitions/uptime-kuma/uptime-kuma.lisp
@@ -1,9 +1,9 @@
 (define-app
     (version "1.23.15")
     (ports 3001)
-        (containers
-            (container
-                (name "uptime-kuma")
-                (image "docker.io/louislam/uptime-kuma:1")
-                (volumes 
-                    ("data" "/app/data")))))
+    (containers
+        (container
+            (name "uptime-kuma")
+            (image "docker.io/louislam/uptime-kuma:1")
+            (volumes
+                ("data" "/app/data")))))
