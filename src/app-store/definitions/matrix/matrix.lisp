86f83782eb70d21419c2d81fd1c37fc15e200fad
Sasank Chilamkurthy <sasank@von-neumann.ai>
2024-11-04 15:01:34 +0530

    add matrix whatsapp integration - but half assed

--- src/app-store/definitions/matrix/matrix.lisp
+++ src/app-store/definitions/matrix/matrix.lisp
@@ -3,7 +3,8 @@
     (ports 8008)
     (url "https://github.com/open-webui/open-webui/")
     (let ((server-name ,(interactive-input "server name" "It is important to choose the name for your server before you install Synapse, because it cannot be changed later.\nThe server name determines the \"domain\" part of user-ids for users on your server: these will all be of the format @user:my.domain.name. It also determines how other matrix servers will reach yours for federation."))
-          (db-password ,(gen-password)))
+          (db-password ,(gen-password))
+          (mautrix-db-password ,(gen-password)))
         (containers
             (container
                 (name "synapse")
@@ -26,4 +27,24 @@
                     ("POSTGRESQL_USERNAME" "synapse")
                     ("POSTGRESQL_DATABASE" "synapse")
                     ("POSTGRESQL_INITDB_ARGS" "--encoding=UTF-8 --lc-collate=C --lc-ctype=C")
-                    ("POSTGRESQL_PASSWORD" ,db-password))))))
+                    ("POSTGRESQL_PASSWORD" ,db-password)))
+            (container
+                (name "mautrix-whatsapp")
+                (image "dock.mau.dev/mautrix/whatsapp:v0.11.0")
+                (command "bash /start_mautrix.sh")
+                (volumes
+                    ("mautrix-data" "/data")
+                    ("start_mautrix.sh" "/start_mautrix.sh"))
+                (environment
+                    ("SYNAPSE_SERVER_NAME" ,server-name)
+                    ("MAUTRIX_POSTGRES_PASSWORD" ,mautrix-db-password)))
+            (container
+                (name "mautrix-postgres")
+                (image "docker.io/bitnami/postgresql:17")
+                (volumes
+                    ("mautrix-pgdata" "/bitnami/postgresql"))
+                (environment
+                    ("POSTGRESQL_USERNAME" "mautrix")
+                    ("POSTGRESQL_DATABASE" "mautrix")
+                    ("POSTGRESQL_PORT_NUMBER" "5433")
+                    ("POSTGRESQL_PASSWORD" ,mautrix-db-password))))))
---
+++ src/app-store/definitions/matrix/start_mautrix.sh
@@ -0,0 +1,30 @@
+set -e
+if [ ! -f /data/config.yaml  ]; then
+    echo "Config not found. Generating"
+    /docker-run.sh
+    
+    apk add --no-cache py-yaml
+    python << END
+import os
+import yaml
+
+with open("/data/config.yaml") as f:
+    mautrix_config = yaml.safe_load(f)
+
+mautrix_config['homeserver']['address'] = "http://localhost:8008"
+mautrix_config['homeserver']['domain'] = "$SYNAPSE_SERVER_NAME"
+mautrix_config['database']['uri'] = "postgres://mautrix:$MAUTRIX_POSTGRES_PASSWORD@localhost:5433/mautrix?sslmode=disable"
+
+mautrix_config['bridge']['permissions'] = {
+    '*': 'relay', '$SYNAPSE_SERVER_NAME': 'user', '@admin:$SYNAPSE_SERVER_NAME': 'admin'
+}
+
+with open("/data/config.yaml", "w") as f:
+    yaml.dump(mautrix_config, f, default_flow_style=False, sort_keys=False)
+END
+
+    # generate registration
+    /docker-run.sh
+fi
+
+/docker-run.sh
