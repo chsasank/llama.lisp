633bcc4abbf3ba89b58ffd9a430d56fac38d5141
adi-lb-phoenix <adithyabhatl1998@gmail.com>
2025-01-04 15:39:41 +0530

    there was unnecessary extra code in it, which was result of not verifying before pushing

--- src/app-store/definitions/mattermost/mattermost.lisp
+++ src/app-store/definitions/mattermost/mattermost.lisp
@@ -43,28 +43,5 @@
                     ("mattermost/plugins" "/mattermost/plugins")
                     ("mattermost/client/plugins" "/mattermost/client/plugins")
                     ("mattermost/bleve-indexes" "/mattermost/bleve-indexes")
-                    ("init_dirs.sh" "/mnt/init_dirs.sh"))))))       t/data" "/mattermost/data")
-                    ("mattermost/logs" "/mattermost/logs")
-                    ("mattermost/plugins" "/mattermost/plugins")
-                    ("mattermost/client/plugins" "/mattermost/client/plugins")
-                    ("mattermost/bleve-indexes" "/mattermost/bleve-indexes"))
-                (environment
-                    ("DOMAIN" "mm.example.com")
-                    ("TZ" "UTC")
-                    ("MM_SQLSETTINGS_DRIVERNAME" "postgres")
-                    ("MM_SQLSETTINGS_DATASOURCE" 
-                        ,(format "postgres://mmuser:{}@localhost:5432/mattermost?sslmode=disable&connect_timeout=10" ,db-password))
-                    ("MM_BLEVESETTINGS_INDEXDIR" "/mattermost/bleve-indexes")
-                    ("MATTERMOST_CONTAINER_READONLY" "false")))
-            (container
-                (name "init")
-                (image "docker.io/library/ubuntu:22.04")
-                (command "sh /mnt/init_dirs.sh")
-                (volumes
-                    ("mattermost/config" "/mattermost/config") 
-                    ("mattermost/data" "/mattermost/data")
-                    ("mattermost/logs" "/mattermost/logs")
-                    ("mattermost/plugins" "/mattermost/plugins")
-                    ("mattermost/client/plugins" "/mattermost/client/plugins")
-                    ("mattermost/bleve-indexes" "/mattermost/bleve-indexes")
-                    ("init_dirs.sh" "/mnt/init_dirs.sh"))))))       
+                    ("init_dirs.sh" "/mnt/init_dirs.sh"))))))
+
