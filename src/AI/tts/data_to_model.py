c841d3f8cddd0463b44b29fbeb76db9e18d18811
adi-lb-phoenix <adithyabhatl1998@gmail.com>
2025-06-28 20:52:04 +0530

    this commit has gone through black which is for good coding practices w.r.t spacing and neat looking code

--- src/AI/tts/data_to_model.py
+++ src/AI/tts/data_to_model.py
@@ -2,7 +2,7 @@ import json
 import glob
 import os
 import random
-import requests 
+import requests
 from tqdm.auto import tqdm
 from indicnlp.tokenize import sentence_tokenize
 from datasets import Dataset, Audio, load_dataset, load_from_disk
@@ -17,22 +17,36 @@ def random_sentences(files_list, sample_size):
     random_files_list = random.sample(files_list, sample_size)
     sentence_list = []
     for filename in random_files_list:
-        with open(filename, 'r') as file_pointer:
-            file_contents = json.load(file_pointer)   
-            indic_string = file_contents['text']
-            sentence_list = sentence_list + sentence_tokenize.sentence_split(indic_string, "ka")
+        with open(filename, "r") as file_pointer:
+            file_contents = json.load(file_pointer)
+            indic_string = file_contents["text"]
+            sentence_list = sentence_list + sentence_tokenize.sentence_split(
+                indic_string, "ka"
+            )
     return sentence_list
-    
+
+
 def data_set_creation(sentence_list, n_samples):
     data_set_list = []
     random_idxs = random.sample(range(len(sentence_list)), n_samples)
     for sentence_indices in random_idxs:
-        sent_length = random.choice([1,2,3])
-        data_set_list.append(' '.join(sentence_list[sentence_indices: sentence_indices + sent_length]))
+        sent_length = random.choice([1, 2, 3])
+        data_set_list.append(
+            " ".join(sentence_list[sentence_indices : sentence_indices + sent_length])
+        )
     return data_set_list
 
-def gcp_tts(line, fname, language_code ="te-IN", voice_name ="te-IN-Chirp3-HD-Achernar", gender = "FEMALE"):
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/phoenix/learning/AI/tts_google/telugu/tts_gcp.json"
+
+def gcp_tts(
+    line,
+    fname,
+    language_code="te-IN",
+    voice_name="te-IN-Chirp3-HD-Achernar",
+    gender="FEMALE",
+):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = (
+        "/home/phoenix/learning/AI/tts_google/telugu/tts_gcp.json"
+    )
     # Instantiates a client
     client = texttospeech.TextToSpeechClient()
     # Set the text input to be synthesized
@@ -40,11 +54,14 @@ def gcp_tts(line, fname, language_code ="te-IN", voice_name ="te-IN-Chirp3-HD-Ac
 
     # Build the voice request, select the language code ("kn-IN") and the ssml
     # voice gender ("FEMALE")
-    #texttospeech.SsmlVoiceGender.FEMALE
-    s_ssml_gender = f'texttospeech.SsmlVoiceGender.{gender}'
+    # texttospeech.SsmlVoiceGender.FEMALE
+    s_ssml_gender = f"texttospeech.SsmlVoiceGender.{gender}"
     voice = texttospeech.VoiceSelectionParams(
-        language_code = language_code, name = voice_name ,ssml_gender=texttospeech.SsmlVoiceGender.FEMALE )
-    
+        language_code=language_code,
+        name=voice_name,
+        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE,
+    )
+
     # Select the type of audio file you want returned
     audio_config = texttospeech.AudioConfig(
         audio_encoding=texttospeech.AudioEncoding.MP3
@@ -61,28 +78,31 @@ def gcp_tts(line, fname, language_code ="te-IN", voice_name ="te-IN-Chirp3-HD-Ac
         # Write the response to the output file.
         out.write(response.audio_content)
 
+
 if __name__ == "__main__":
 
     data_dir = "extracted_data/"
     files_list = glob.glob(os.path.join(data_dir, "*.json"))
-    print("total_files are",len(files_list))
+    print("total_files are", len(files_list))
     sample_size = 1000
     n_samples = 1500
-    fname_to_save = 'data_set_list.json'
+    fname_to_save = "data_set_list.json"
 
     try:
-        with open(fname_to_save, 'r') as f:
+        with open(fname_to_save, "r") as f:
             data_set_list = json.load(f)
     except FileNotFoundError:
         sentence_list = random_sentences(files_list, sample_size)
         data_set_list = data_set_creation(sentence_list, n_samples)
 
-        with open(fname_to_save, 'w') as f:
+        with open(fname_to_save, "w") as f:
             json.dump(data_set_list, f)
 
-    print("Enter the language . For example for kannada it is kn-IN, for Telugu it is te-IN")
+    print(
+        "Enter the language . For example for kannada it is kn-IN, for Telugu it is te-IN"
+    )
     language_code = input()
- 
+
     print("enter the name of the language")
     language_name = input()
     print("enter the name of the artist")
@@ -90,7 +110,6 @@ if __name__ == "__main__":
 
     print("enter gender of the speaker")
     gender = input()
-
 
     for count, sent in enumerate(tqdm(data_set_list)):
         fname = f"generated_tts/{count}.mp3"
@@ -102,26 +121,34 @@ if __name__ == "__main__":
             except:
                 print(f"error with {sent}")
 
-
-
-    audio_prompts_dict = json.load(open('./data_set_list.json'))
-    audio_files = sorted(glob.glob('generated_tts/*.mp3'))
-    file_indices = [int(os.path.basename(x)[:-len('.mp3')]) for x in audio_files]
+    audio_prompts_dict = json.load(open("./data_set_list.json"))
+    audio_files = sorted(glob.glob("generated_tts/*.mp3"))
+    file_indices = [int(os.path.basename(x)[: -len(".mp3")]) for x in audio_files]
     audio_prompts = [audio_prompts_dict[x] for x in file_indices]
     speaker = voice_name
 
-    #creating audio dataset and pushing it to huggingh face. 
-    ds = Dataset.from_dict({
-        "audio": audio_files,
-        "prompt": audio_prompts,
-        "description": [f'{speaker} speaks {language_name} in a natural tone'] * len(audio_prompts)
-    }).cast_column("audio", Audio()).train_test_split(seed=0)
-    ds.save_to_disk(f'tts_ka_dataset_{language_code}_{language_name}')
-    ds.push_to_hub(f'chsasank/tts_synthetic_{language_code}_{language_name}') 
-    HF_dataset_name = f'chsasank/tts_synthetic_{language_code}_{language_name}'
+    # creating audio dataset and pushing it to huggingh face.
+    ds = (
+        Dataset.from_dict(
+            {
+                "audio": audio_files,
+                "prompt": audio_prompts,
+                "description": [f"{speaker} speaks {language_name} in a natural tone"]
+                * len(audio_prompts),
+            }
+        )
+        .cast_column("audio", Audio())
+        .train_test_split(seed=0)
+    )
+    ds.save_to_disk(f"tts_ka_dataset_{language_code}_{language_name}")
+    ds.push_to_hub(f"chsasank/tts_synthetic_{language_code}_{language_name}")
+    HF_dataset_name = f"chsasank/tts_synthetic_{language_code}_{language_name}"
     print("the training should start")
-    print(f'accelerate launch ./parler-tts/training/run_parler_tts_training.py     --model_name_or_path "ai4bharat/indic-parler-tts"     --report_to "wandb"     --overwrite_output_dir true     --train_dataset_name  {HF_dataset_name}    --train_metadata_dataset_name "chsasank/tts_synthetic"     --train_dataset_config_name "default"     --train_split_name "train"     --eval_dataset_name "chsasank/tts_synthetic"     --eval_dataset_config_name "default"     --eval_metadata_dataset_name "chsasank/tts_synthetic"     --eval_split_name "test"     --max_eval_samples 8     --per_device_eval_batch_size 8     --target_audio_column_name "audio"     --description_column_name "description"     --prompt_column_name "prompt"     --max_duration_in_seconds 20     --min_duration_in_seconds 2.0     --max_text_length 400     --preprocessing_num_workers 2     --do_train true     --num_train_epochs 2     --gradient_accumulation_steps 18     --gradient_checkpointing true     --per_device_train_batch_size 2     --learning_rate 0.0001     --adam_beta1 0.9     --adam_beta2 0.99     --weight_decay 0.01     --lr_scheduler_type "constant_with_warmup"     --warmup_steps 50     --logging_steps 2     --freeze_text_encoder true     --audio_encoder_per_device_batch_size 5     --dtype "float16"     --seed 456     --output_dir "./output_dir_training/"     --temporary_save_to_disk "./audio_code_tmp/"     --save_to_disk "./tmp_dataset_audio/"     --dataloader_num_workers 2     --do_eval     --pred')
-    os.system(f"""
+    print(
+        f'accelerate launch ./parler-tts/training/run_parler_tts_training.py     --model_name_or_path "ai4bharat/indic-parler-tts"     --report_to "wandb"     --overwrite_output_dir true     --train_dataset_name  {HF_dataset_name}    --train_metadata_dataset_name "chsasank/tts_synthetic"     --train_dataset_config_name "default"     --train_split_name "train"     --eval_dataset_name "chsasank/tts_synthetic"     --eval_dataset_config_name "default"     --eval_metadata_dataset_name "chsasank/tts_synthetic"     --eval_split_name "test"     --max_eval_samples 8     --per_device_eval_batch_size 8     --target_audio_column_name "audio"     --description_column_name "description"     --prompt_column_name "prompt"     --max_duration_in_seconds 20     --min_duration_in_seconds 2.0     --max_text_length 400     --preprocessing_num_workers 2     --do_train true     --num_train_epochs 2     --gradient_accumulation_steps 18     --gradient_checkpointing true     --per_device_train_batch_size 2     --learning_rate 0.0001     --adam_beta1 0.9     --adam_beta2 0.99     --weight_decay 0.01     --lr_scheduler_type "constant_with_warmup"     --warmup_steps 50     --logging_steps 2     --freeze_text_encoder true     --audio_encoder_per_device_batch_size 5     --dtype "float16"     --seed 456     --output_dir "./output_dir_training/"     --temporary_save_to_disk "./audio_code_tmp/"     --save_to_disk "./tmp_dataset_audio/"     --dataloader_num_workers 2     --do_eval     --pred'
+    )
+    os.system(
+        f"""
     accelerate launch ./parler-tts/training/run_parler_tts_training.py \
     --model_name_or_path "ai4bharat/indic-parler-tts" \
     --report_to "wandb" \
@@ -166,20 +193,30 @@ if __name__ == "__main__":
     --do_eval \
     --predict_with_generate \
     --include_inputs_for_metrics \
-    --group_by_length true""")
+    --group_by_length true"""
+    )
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    model = ParlerTTSForConditionalGeneration.from_pretrained('./parler-tts/output_dir_training/').to(device)
+    model = ParlerTTSForConditionalGeneration.from_pretrained(
+        "./parler-tts/output_dir_training/"
+    ).to(device)
     tokenizer = AutoTokenizer.from_pretrained("ai4bharat/indic-parler-tts")
-    description_tokenizer = AutoTokenizer.from_pretrained(model.config.text_encoder._name_or_path)
-    
-    print(f'Enter the input prompt in {language_code}')
+    description_tokenizer = AutoTokenizer.from_pretrained(
+        model.config.text_encoder._name_or_path
+    )
+
+    print(f"Enter the input prompt in {language_code}")
     prompt = input()
-    description = f'{speaker} speaks {language_name} in a natural tone'
-    description_input_ids = description_tokenizer(description, return_tensors="pt").to(device)
+    description = f"{speaker} speaks {language_name} in a natural tone"
+    description_input_ids = description_tokenizer(description, return_tensors="pt").to(
+        device
+    )
     prompt_input_ids = tokenizer(prompt, return_tensors="pt").to(device)
 
-    generation = model.generate(input_ids=description_input_ids.input_ids, attention_mask=description_input_ids.attention_mask, prompt_input_ids=prompt_input_ids.input_ids, prompt_attention_mask=prompt_input_ids.attention_mask)
+    generation = model.generate(
+        input_ids=description_input_ids.input_ids,
+        attention_mask=description_input_ids.attention_mask,
+        prompt_input_ids=prompt_input_ids.input_ids,
+        prompt_attention_mask=prompt_input_ids.attention_mask,
+    )
     audio_arr = generation.cpu().numpy().squeeze()
     sf.write("indic_tts_out.wav", audio_arr, model.config.sampling_rate)
-
-
--- src/AI/tts/dewiki_functions.py
+++ src/AI/tts/dewiki_functions.py
@@ -5,12 +5,14 @@ from html2text import html2text as htt
 import wikitextparser as wtp
 import glob
 import os
+
+
 def dewiki(text):
-    text = wtp.parse(text).plain_text()  # wiki to plaintext 
+    text = wtp.parse(text).plain_text()  # wiki to plaintext
     text = htt(text)  # remove any HTML
-    text = text.replace('\\n',' ')  # replace newlines
-    text = re.sub('\s+', ' ', text)  # replace excess whitespace
-    text = re.sub('=+', "=", text) ##
+    text = text.replace("\\n", " ")  # replace newlines
+    text = re.sub("\s+", " ", text)  # replace excess whitespace
+    text = re.sub("=+", "=", text)  ##
     return text
 
 
@@ -18,17 +20,19 @@ def analyze_chunk(text):
     try:
         if '<redirect title="' in text:  # this is not the main article
             return None
-        if '(disambiguation)' in text:  # this is not an article
+        if "(disambiguation)" in text:  # this is not an article
             return None
         else:
-            title = text.split('<title>')[1].split('</title>')[0]
+            title = text.split("<title>")[1].split("</title>")[0]
             title = htt(title)
-            if ':' in title:  # most articles with : in them are not articles we care about
+            if (
+                ":" in title
+            ):  # most articles with : in them are not articles we care about
                 return None
-        serial = text.split('<id>')[1].split('</id>')[0]
-        content = text.split('</text')[0].split('<text')[1].split('>', maxsplit=1)[1]
+        serial = text.split("<id>")[1].split("</id>")[0]
+        content = text.split("</text")[0].split("<text")[1].split(">", maxsplit=1)[1]
         content = dewiki(content)
-        return {'title': title.strip(), 'text': content.strip(), 'id': serial.strip()}
+        return {"title": title.strip(), "text": content.strip(), "id": serial.strip()}
     except Exception as oops:
         print(oops)
         return None
@@ -37,26 +41,26 @@ def analyze_chunk(text):
 def save_article(article, savedir):
     doc = analyze_chunk(article)
     if doc:
-        print('SAVING:', doc['title'])
-        filename = doc['id'] + '.json'
-        with open(savedir + filename, 'w', encoding='utf-8') as outfile:
+        print("SAVING:", doc["title"])
+        filename = doc["id"] + ".json"
+        with open(savedir + filename, "w", encoding="utf-8") as outfile:
             json.dump(doc, outfile, sort_keys=True, indent=1, ensure_ascii=False)
 
 
 def process_file_text(filename, savedir):
-    article = ''
-    with open(filename, 'r', encoding='utf-8') as infile:
+    article = ""
+    with open(filename, "r", encoding="utf-8") as infile:
         for line in infile:
-            if '<page>' in line:
-                article = ''
-            elif '</page>' in line:  # end of article
+            if "<page>" in line:
+                article = ""
+            elif "</page>" in line:  # end of article
                 Thread(target=save_article, args=(article, savedir)).start()
             else:
                 article += line
 
-if __name__ == "__main__":
-	filename = glob.glob('*.xml')
-	print(filename)
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	process_file_text(filename[0],f'{dir_path}/extracted_data/')
 
+if __name__ == "__main__":
+    filename = glob.glob("*.xml")
+    print(filename)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    process_file_text(filename[0], f"{dir_path}/extracted_data/")
