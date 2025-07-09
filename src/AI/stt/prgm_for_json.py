77143bdd45e2244d12f365fb8c589091ec46b796
adi-lb-phoenix <adithyabhatl1998@gmail.com>
2025-07-09 13:48:43 +0530

    ran black through all the .py files

--- src/AI/stt/prgm_for_json.py
+++ src/AI/stt/prgm_for_json.py
@@ -1,15 +1,16 @@
 import json
+
 file_list = {
     "url_kannada_movies_list.txt": "movie",
     "url_news_playlist.txt": "news",
-    "url_serial_playlist.txt": "serial"
+    "url_serial_playlist.txt": "serial",
 }
 
 data = []
 for file_name, data_type in file_list.items():
-    with open(file_name, "r") as f: 
-        lines = f.read().strip().split('\n')
-        data.extend([{'video': line, 'type': data_type} for line in lines])
+    with open(file_name, "r") as f:
+        lines = f.read().strip().split("\n")
+        data.extend([{"video": line, "type": data_type} for line in lines])
 
-with open("kannada_asr_data.json", 'w') as f:
+with open("kannada_asr_data.json", "w") as f:
     json.dump(data, f)
--- src/AI/stt/training.py
+++ src/AI/stt/training.py
@@ -1,22 +1,28 @@
-
 import pkg_resources
 import os
+
 pkg_resources.require("transformers==4.48.0")
 os.system("pip install -U 'protobuf>=3.4.0'")
 import transformers
 from huggingface_hub import notebook_login
 from datasets import load_dataset, DatasetDict
-from transformers import WhisperFeatureExtractor 
+from transformers import WhisperFeatureExtractor
 from transformers import WhisperTokenizer
 from transformers import WhisperForConditionalGeneration
 import torch
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
 import evaluate
-from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer, WhisperTokenizer, WhisperProcessor
-from datasets import Audio 
+from transformers import (
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+    WhisperTokenizer,
+    WhisperProcessor,
+)
+from datasets import Audio
 from transformers import pipeline
 import gradio as gr
+
 
 def sample(ds, percentage=1):
     if percentage >= 1:
@@ -24,7 +30,8 @@ def sample(ds, percentage=1):
 
     return ds.select(range(int(percentage * len(ds))))
 
-#os.system("huggingface-cli ....." mind that you should be logged into hugging face either through the script or 
+
+# os.system("huggingface-cli ....." mind that you should be logged into hugging face either through the script or
 HF_dataset_name = "adithyal1998Bhat/stt_synthetic_kn-IN_kannada"
 common_voice = DatasetDict()
 common_voice["train"] = sample(load_dataset(HF_dataset_name, split="train"))
@@ -33,19 +40,27 @@ common_voice["test"] = sample(load_dataset(HF_dataset_name, split="test"), 0.05)
 base_model_name = "openai/whisper-medium"
 feature_extractor = WhisperFeatureExtractor.from_pretrained(base_model_name)
 
-#what does the tokenize do here
-tokenizer = WhisperTokenizer.from_pretrained(base_model_name, language="Kannada", task="transcribe")
-processor = WhisperProcessor.from_pretrained(base_model_name, language="Kannada", task="transcribe")
+# what does the tokenize do here
+tokenizer = WhisperTokenizer.from_pretrained(
+    base_model_name, language="Kannada", task="transcribe"
+)
+processor = WhisperProcessor.from_pretrained(
+    base_model_name, language="Kannada", task="transcribe"
+)
 common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
+
 
 def prepare_dataset(batch):
     audio = batch["audio"]
-    #compute log-mel input features from input  audio array. Why?
-    batch["input_features"] = feature_extractor(audio["array"],sampling_rate=audio["sampling_rate"]).input_features[0]
+    # compute log-mel input features from input  audio array. Why?
+    batch["input_features"] = feature_extractor(
+        audio["array"], sampling_rate=audio["sampling_rate"]
+    ).input_features[0]
 
-    #encode target text to label ids
+    # encode target text to label ids
     batch["labels"] = tokenizer(batch["prompt"]).input_ids
-    return batch 
+    return batch
+
 
 common_voice = common_voice.map(prepare_dataset, num_proc=2, writer_batch_size=20)
 
@@ -56,13 +71,13 @@ model = WhisperForConditionalGeneration.from_pretrained(base_model_name)
 def drop_large_sentences(ds):
     # drop samples with tokens supported by the model
     max_size = model.config.max_target_positions
-    idxs_small = [idx for idx, d in enumerate(ds['labels']) if len(d) < max_size]
+    idxs_small = [idx for idx, d in enumerate(ds["labels"]) if len(d) < max_size]
     print("dropping {} samples with large sentences".format(len(ds) - len(idxs_small)))
     return ds.select(idxs_small)
 
 
-common_voice['train'] = drop_large_sentences(common_voice['train'])
-common_voice['test'] = drop_large_sentences(common_voice['test'])
+common_voice["train"] = drop_large_sentences(common_voice["train"])
+common_voice["test"] = drop_large_sentences(common_voice["test"])
 print(common_voice)
 
 
@@ -70,29 +85,43 @@ model.generation_config.language = "Kannada"
 model.generation_config.task = "transcribe"
 model.generation_config.forced_decoder_ids = None
 
+
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
     processor: Any
     decoder_start_token_id: int
 
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        input_features = [{"input_features": feature["input_features"]} for feature in features]
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        input_features = [
+            {"input_features": feature["input_features"]} for feature in features
+        ]
+        batch = self.processor.feature_extractor.pad(
+            input_features, return_tensors="pt"
+        )
 
         label_features = [{"input_ids": feature["labels"]} for feature in features]
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
 
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
 
-        #what is bos?
-        if(labels[:,0] == self.decoder_start_token_id).all().cpu().item():
+        # what is bos?
+        if (labels[:, 0] == self.decoder_start_token_id).all().cpu().item():
             labels = labels[:, 1:]
 
         batch["labels"] = labels
         return batch
-    
-data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor, decoder_start_token_id=model.config.decoder_start_token_id,)
+
+
+data_collator = DataCollatorSpeechSeq2SeqWithPadding(
+    processor=processor,
+    decoder_start_token_id=model.config.decoder_start_token_id,
+)
 metric = evaluate.load("wer")
+
 
 def compute_metrics(pred):
     pred_ids = pred.predictions
@@ -100,12 +129,12 @@ def compute_metrics(pred):
 
     label_ids[label_ids == -100] = tokenizer.pad_token_id
 
-    #no grouping token when we are computing metrics
+    # no grouping token when we are computing metrics
     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
 
     wer = 100 * metric.compute(predictions=pred_str, references=label_str)
-    return {"wer":wer}
+    return {"wer": wer}
 
 
 training_args = Seq2SeqTrainingArguments(
@@ -131,15 +160,16 @@ training_args = Seq2SeqTrainingArguments(
     push_to_hub=True,
 )
 trainer = Seq2SeqTrainer(
-    args = training_args,
+    args=training_args,
     model=model,
     train_dataset=common_voice["train"],
     eval_dataset=common_voice["test"],
     data_collator=data_collator,
     compute_metrics=compute_metrics,
-    tokenizer=processor.feature_extractor,)
+    tokenizer=processor.feature_extractor,
+)
 
- 
+
 processor.save_pretrained(training_args.output_dir)
 trainer.train(resume_from_checkpoint=True)
 
@@ -154,4 +184,3 @@ kwargs = {
 }
 
 trainer.push_to_hub(**kwargs)
-
