f7639d973571983987a0621cd5efd5e3a0242a5f
tinakumarj <tinakumarj01@gmail.com>
2025-08-11 21:08:46 +0530

    frigate using lisp

--- src/app-store/definitions/frigate/frigate.lisp
+++ src/app-store/definitions/frigate/frigate.lisp
@@ -1,14 +1,12 @@
 (define-app
   (version "3.9")
-  (ports 8971 8554 8555/tcp 8555/udp)
+  (ports 8971)
   (containers
     (container
       (name "frigate")
       (image "ghcr.io/blakeblackshear/frigate:stable")
-      (restart "unless-stopped")
-      (devices
-        ("/dev/bus/usb" "/dev/bus/usb"))
       (volumes
         ("/etc/localtime" "/etc/localtime:ro")
-        ("./config" "/config")
-        ("./storage" "/media/frigate"))))))
+        ("config" "/config")
+        ("storage" "/media/frigate/storage"))
+      (additional-flags "--device nvidia.com/gpu=all"))))
