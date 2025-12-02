6841f3964518ff717b4a66176bfe4bf7fa3ed2fe
jonusthummala <jonusthummala18099@gmail.com>
2025-12-02 12:15:09 +0530

    Addded MSSQL source db to script

--- src/database/etl/tests/data/create_testing_db.sh
+++ src/database/etl/tests/data/create_testing_db.sh
@@ -56,3 +56,45 @@ podman run -d --replace \
 
 sleep 5
 echo "==> created $TEST_TARGET_DB"
+
+# Creating MSSQL database
+TEST_SOURCE_DB=etl-test-source-mssql-db
+MSSQL_PASSWORD=Intelarc@123
+podman kill $TEST_SOURCE_DB && podman rm $TEST_SOURCE_DB || true
+podman run -d --replace \
+    --name $TEST_SOURCE_DB \
+    -e ACCEPT_EULA=Y \
+    -e MSSQL_SA_PASSWORD=$MSSQL_PASSWORD \
+    -e MSSQL_PID=Developer \
+    -p 1433:1433 \
+    mcr.microsoft.com/mssql/server:2025-latest
+
+# After the Database got create use below command for login to database
+# podman exec -it etl-test-target-mssql-db /opt/mssql-tools18/bin/sqlcmd -No -S localhost -U SA -P "Intelarc@123"
+
+# Creating directory inside the container
+podman exec -it $TEST_SOURCE_DB mkdir /var/opt/mssql/backup
+
+# It will download the file if not exists
+SCRIPT_DIR="$(pwd)"
+
+if [ -f "$SCRIPT_DIR/wwi.bak" ]; then
+    echo "wwi.bak already exists → skipping download."
+else
+    echo "wwi.bak not found → downloading..."
+    curl -L -o "$SCRIPT_DIR/wwi.bak" \
+      'https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak'
+fi
+
+sleep 10
+
+# To copy the file to the container
+podman cp "$SCRIPT_DIR/wwi.bak" $TEST_SOURCE_DB:/var/opt/mssql/backup
+
+# To restore the database inside the container
+podman exec -it $TEST_SOURCE_DB /opt/mssql-tools18/bin/sqlcmd -No \
+   -S localhost -U sa -P $MSSQL_PASSWORD \
+   -Q 'RESTORE DATABASE WideWorldImporters FROM DISK = "/var/opt/mssql/backup/wwi.bak" WITH MOVE "WWI_Primary" TO "/var/opt/mssql/data/WideWorldImporters.mdf", MOVE "WWI_UserData" TO "/var/opt/mssql/data/WideWorldImporters_userdata.ndf", MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "WWI_InMemory_Data_1" TO "/var/opt/mssql/data/WideWorldImporters_InMemory_Data_1"'
+
+sleep 5
+echo "==> created $TEST_SOURCE_DB"
