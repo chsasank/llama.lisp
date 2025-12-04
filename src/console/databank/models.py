cee81b1495ae3f6b227ca6a51382e87c90c11c67
Sasank Chilamkurthy <sasank@von-neumann.ai>
2025-12-04 16:18:51 +0530

    add etl models

--- src/console/databank/models.py
+++ src/console/databank/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
 # Create your models here.
+class DatabaseConfiguration(models.Model):
+    class ETLType(models.TextChoices):
+        SOURCE = "source"
+        TARGET = "target"
+
+    class DBTypes(models.TextChoices):
+        POSTGRES = "postgres"
+        CLICKHOUSE = "clickhouse"
+        MSSQL = "mssql"
+        ORACLE = "oracle"
+
+    etl_type = models.CharField(choices=ETLType.choices)
+    database_type = models.CharField(choices=DBTypes.choices)
+    connection_config = models.JSONField()
+
+
+class ETLConfiguration(models.Model):
+    source_database = models.ForeignKey(
+        DatabaseConfiguration, on_delete=models.CASCADE, related_name="source"
+    )
+    target_database = models.ForeignKey(
+        DatabaseConfiguration, on_delete=models.CASCADE, related_name="target"
+    )
+
+    source_table = models.CharField()
+    target_table = models.CharField()
+    replication_key = models.CharField(null=True)
+    replication_state = models.JSONField(null=True)
+
+    def run_etl(self):
+        pass
--- src/console/databank/tests.py
+++ src/console/databank/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
+from .models import DatabaseConfiguration, ETLConfiguration
+
+
 # Create your tests here.
+class DatabaseConfigurationModelTests(TestCase):
+    def test_model_creation(self):
+        db_config = DatabaseConfiguration.objects.create(
+            etl_type="source",
+            database_type="postgres",
+            connection_config={"host": "localhost", "blah": "blah"},
+        )
+        db_config.save()
+
+        for existing_db_config in DatabaseConfiguration.objects.all():
+            assert existing_db_config.database_type == "postgres"
+
+        # note that choices are just for documentation
+        db_config = DatabaseConfiguration.objects.create(
+            etl_type="source_llololol",
+            database_type="postgres",
+            connection_config={"host": "localhost", "blah": "blah"},
+        )
+        db_config.save()
+
+
+class ETLConfigurationModelTests(TestCase):
+    def test_model_creation(self):
+        source_db_config = DatabaseConfiguration.objects.create(
+            etl_type="source",
+            database_type="postgres",
+            connection_config={"host": "localhost", "blah": "blah"},
+        )
+        target_db_config = DatabaseConfiguration.objects.create(
+            etl_type="target",
+            database_type="postgres",
+            connection_config={"host": "localhost", "blah": "blah"},
+        )
+        etl_config = ETLConfiguration.objects.create(
+            source_database=source_db_config,
+            target_database=target_db_config,
+            source_table="table_from",
+            target_table="table_to",
+        )
+        etl_config.save()
+        assert etl_config.source_database.etl_type == "source"
