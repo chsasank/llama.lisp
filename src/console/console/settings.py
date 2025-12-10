0e3447bcb9053b85aa21290bcfe6a2eec5539bab
jonusthummala <jonusthummala18099@gmail.com>
2025-12-10 14:24:58 +0530

    Updated Login UI and added Logout Button

--- src/console/console/settings.py
+++ src/console/console/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
